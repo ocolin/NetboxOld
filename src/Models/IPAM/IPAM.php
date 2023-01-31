00490bb88bc5cb7bb5e3121c9b145770fa044e40
Colin Miller <ocolin@cruzio.com>
2023-01-31 15:14:59 -0500

    Updated IPAM model core

--- src/Models/IPAM/IPAM.php
+++ src/Models/IPAM/IPAM.php
@@ -4,7 +4,16 @@ declare( strict_types = 1 );
 
 namespace Cruzio\Netbox\Models\IPAM;
 
+use Cruzio\Netbox\Models\HTTP;
+
 class IPAM
 {
+    protected HTTP $http;
+
     protected $uri = 'IPAM/';
+
+    public function __construct( HTTP $http = null )
+    {
+        $this->http = $http ?? new HTTP();
+    }
 }
