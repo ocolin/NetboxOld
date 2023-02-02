a6bd33abf2e411f3374eab87cd91a26a4d326e77
Colin Miller <ocolin@cruzio.com>
2023-02-01 19:27:07 -0500

    Added model VirtualChassis and tests

---
+++ tests/models/dcim/testVirtualChassis.php
@@ -0,0 +1,351 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Cruzio\Netbox\Models\DCIM;
+
+use Cruzio\Netbox\Models\testCore;
+
+require_once __DIR__ . '/../testCore.php';
+
+class testVirtualChassis extends testCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+/* TEST OPTIONS
+---------------------------------------------------------------------------- */
+
+    public function testOptions()
+    {
+        $o = new VirtualChassis();
+        $result = $o->options();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'name', $result['body'] );
+    }
+
+
+
+/* TEST GET DETAIL
+---------------------------------------------------------------------------- */
+
+public function testGetDetail() : void
+{
+    // SETUP
+    $vchas = $this->postDetail()['body'];
+
+    $o = new VirtualChassis();
+    $result = $o->getDetail( id: $vchas->id );
+    
+    $this->assertIsArray( $result );
+    $this->assertArrayHasKey( 'status',  $result );
+    $this->assertArrayHasKey( 'headers', $result );
+    $this->assertArrayHasKey( 'body',    $result );
+    $this->assertIsInt( $result['status'] );
+    $this->assertEquals( 200, $result['status'] );
+    $this->assertIsArray( $result['headers'] );
+    $this->assertIsObject( $result['body'] );
+    $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+    // CLEAN UP
+    $this->deleteDetail( $vchas->id );
+}
+
+
+
+/* TEST GET LIST
+---------------------------------------------------------------------------- */
+
+public function testGetList() : void
+{
+    // SETUP
+    $vchas = $this->postDetail()['body'];
+
+    $o = new VirtualChassis();
+    $result = $o->getList();
+
+    $this->assertIsArray( $result );
+    $this->assertArrayHasKey( 'status',  $result );
+    $this->assertArrayHasKey( 'headers', $result );
+    $this->assertArrayHasKey( 'body',    $result );
+    $this->assertIsInt( $result['status'] );
+    $this->assertEquals( 200, $result['status'] );
+    $this->assertIsArray( $result['headers'] );
+    $this->assertIsObject( $result['body'] );
+    $this->assertObjectHasAttribute( 'results', $result['body'] );
+    $this->assertIsArray( $result['body']->results );
+    $this->assertObjectHasAttribute( 'id', $result['body']->results[0] );
+
+     // CLEAN UP
+     $this->deleteDetail( $vchas->id );
+}
+
+
+
+/* TEST POST DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPostDetail() : void
+    {
+        $o = new VirtualChassis();
+        $result = $this->postDetail();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        //CLEAN UP
+        $this->deleteDetail( $result['body']->id );
+    }
+
+
+
+/* TEST POST LIST
+---------------------------------------------------------------------------- */
+
+    public function testPostList() :void
+    {
+        $o = new VirtualChassis();
+        $result = $o->postList(
+        data: [
+            [ 'name' => 'testVirtualChassis1' ],
+            [ 'name' => 'testVirtualChassis2' ],
+        ]  
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+
+        //CLEAN UP
+        foreach( $result['body'] AS $vchas )
+        {
+            $this->deleteDetail( id: $vchas->id );
+        }
+    }
+
+
+
+/* TEST PUT DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPutDetail() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+
+        $o = new VirtualChassis();
+        $result = $o->putDetail( 
+              id: $vchas->id, 
+            name: 'updateVirtualChassis', 
+        );
+        
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $vchas->id );
+    }
+
+
+
+/* TEST PUT LIST
+---------------------------------------------------------------------------- */
+
+    public function testPutList() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+
+        $o = new VirtualChassis();
+        $result = $o->putList(
+            data: [
+                [ 
+                    'id'   => $vchas->id, 
+                    'name' => 'putVirtualChassis',
+                ]
+            ]
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $vchas->id );
+    }
+
+
+
+/* TEST PATCH DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPatchDetail() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+
+        $o = new VirtualChassis();
+        $result = $o->patchDetail(
+              id: $vchas->id,
+            name: 'patchVirtualChassis',
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+
+        // CLEAN UP
+        $this->deleteDetail( $vchas->id );
+    }
+
+
+
+/* TEST PATCH LIST
+---------------------------------------------------------------------------- */
+
+    public function testPatchList() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+
+        $o = new VirtualChassis();
+        $result = $o->patchList(
+            data: [
+                [ 
+                       'id' => $vchas->id, 
+                     'name' => 'patchVirtualChassis',
+                ]
+            ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $vchas->id );
+    }
+
+
+
+
+/* TEST DELETE DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testDeleteDetail() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+        
+        $o = new VirtualChassis();
+        $result = $o->deleteDetail( id: $vchas->id );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* TEST DELETE LIST
+---------------------------------------------------------------------------- */
+
+    public function testDeleteList() : void
+    {
+        // SETUP
+        $vchas = $this->postDetail()['body'];
+
+        $o = new VirtualChassis();
+        $result = $o->deleteList(
+            data: [[ 'id' => $vchas->id ]]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+/* CREATE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function postDetail() : array
+    {
+        $o = new VirtualChassis();
+
+        return $o->postDetail( 
+            name: 'testVirtualChassis',
+            data: [ 
+                'description' => 'PHPUnit test VirtualChassis',
+            ]
+        );
+    }
+
+
+
+/* DELETE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function deleteDetail( int $id )
+    {
+        $o = new VirtualChassis();
+
+        return $o->deleteDetail( id: $id  );
+    }
+
+}
