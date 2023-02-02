36588b4ec873a0a45787b042d416703d0289b3bc
Colin Miller <ocolin@cruzio.com>
2023-02-02 07:30:33 -0500

    Added model ContactRoles and tests

--- src/Models/Tenancy/ContactGroups.php
+++ src/Models/Tenancy/ContactGroups.php
@@ -19,149 +19,104 @@ class ContactGroups extends Tenancy
     }
 
 
-/* GET METHOD
+/* POST METHOD DETAIL
 ---------------------------------------------------------------------------- */
 
 /*
- *
- *
- * @param array $params
- * @param array $headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
+* Create a single Region.
+*
+* @param string $name Name of Region.
+* @param string $slug 
+* @param array  $data optionsl data to be sent
+* @param array $headers HTML request headers
+* @return array Array of HTTP status, headers, and body from Netbox API.
 */
 
-    public function get( array $params = [], array $headers = [] ) : array
-    {
-        if( $id !== 0 ) { $this->uri .= "{$id}/"; }
-
-        return $this->http->get(
-                uri: $this->uri,
-             params: $params,
-            headers: $headers
-        );
-    }
-
-
-
-/* POST METHOD
----------------------------------------------------------------------------- */
-
-/*
- *
- *
- * @param array $data Data to send ibn request
- * @param array $params URI parameters
- * @param array $headers HTML request headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
-*/
-
-    public function post(
-        array $data,
-        array $params  = [],
-        array $headers = []
+    public function postDetail(
+        string $name,
+        string $slug,
+         array $data    = [],
+         array $headers = []
     ) : array
     {
+        $data['name'] = $name;
+        $data['slug'] = $slug;
+
         return $this->http->post(
                 uri: $this->uri,
                body: $data,
-             params: $params,
             headers: $headers
         );
     }
 
 
 
-/* PUT METHOD
+/* PUT METHOD DETAIL
 ---------------------------------------------------------------------------- */
 
 /*
- *
- *
- * @param array $data Data to send ibn request
- * @param array $params URI parameters
- * @param array $headers HTML request headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
+* Update Site
+* 
+* @param integer $id Numerical ID of Region to update.
+* @param string  $name Name of Region to update.
+* @param string  $slug Slug of Region to update.
+* @param array   $data Optional data to send.
+* @param array   $headers HTML request headers
+* @return array Array of HTTP status, headers, and body from Netbox API.
 */
 
-    public function put(
-        array $data,
-        array $params  = [],
-        array $headers = []
+    public function putDetail(
+           int $id,
+        string $name,
+        string $slug,
+         array $data    = [],
+         array $headers = []
     ) : array
     {
-        if( $id !== 0 ) { $this->uri .= "{$id}/"; }
+        $this->uri .= "{$id}/";
+        $data['name'] = $name;
+        $data['slug'] = $slug;
 
         return $this->http->put(
                 uri: $this->uri,
                body: $data,
-             params: $params,
             headers: $headers
         );
     }
 
 
 
-/* PATCH METHOD
+/* PATCH METHOD DETAIL
 ---------------------------------------------------------------------------- */
 
 /*
- *
- *
- * @param array $data Data to send ibn request
- * @param array $params URI parameters
- * @param array $headers HTML request headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
+* Update Region value(s).
+*
+* @param integer $id Numerical ID of Region to update.
+* @param string  $name Name of Region to update.
+* @param string  $slug Slug of Region to update.
+* @param array   $data Optional data to modify.
+* @param array   $headers HTML request headers.
+* @return array Array of HTTP status, headers, and body from Netbox API.
 */
 
-    public function patch(
-        array $data,
-        array $params  = [],
-        array $headers = []
+    public function patchDetail(
+           int $id,
+        string $name,
+        string $slug,
+         array $data    = [],
+         array $headers = []
     ) : array
     {
-        if( $id !== 0 ) { $this->uri .= "{$id}/"; }
+        $this->uri .= "{$id}/";
+        $data['name'] = $name;
+        $data['slug'] = $slug;
+
 
         return $this->http->put(
                 uri: $this->uri,
                body: $data,
-             params: $params,
             headers: $headers
         );
-    }
-
-
-
-/* DELETE METHOD
----------------------------------------------------------------------------- */
-
-/*
- *
- *
- * @param array $headers HTML request headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
-*/
-
-    public function delete( array $headers = [] ) : array
-    {
-        if( $id !== 0 ) { $this->uri .= "{$id}/"; }
-
-        return $this->http->get( uri: $this->uri, headers: $headers );
-    }
-
-
-
-/* OPTIONS METHOD
----------------------------------------------------------------------------- */
-
-/*
- *
- *
- * @param array $headers HTML request headers
- * @return array Array of HTTP status, headers, and body from Netbox API.
-*/
-
-    public function options( array $headers = [] ) : array
-    {
-        return $this->http->options( uri: $this->uri, headers: $headers );
     }
 }
---
+++ tests/models/tenancy/testContactRoles.php
@@ -0,0 +1,360 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Cruzio\Netbox\Models\Tenancy;
+
+use Cruzio\Netbox\Models\testCore;
+
+require_once __DIR__ . '/../testCore.php';
+
+class testContactRoles extends testCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+/* TEST OPTIONS
+---------------------------------------------------------------------------- */
+
+    public function testOptions()
+    {
+        $o = new ContactRoles();
+        $result = $o->options();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'name', $result['body'] );
+    }
+
+
+
+/* TEST GET DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testGetDetail() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->getDetail( id: $role->id );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+/* TEST GET LIST
+---------------------------------------------------------------------------- */
+
+    public function testGetList() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->getList();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'results', $result['body'] );
+        $this->assertIsArray( $result['body']->results );
+        $this->assertObjectHasAttribute( 'id', $result['body']->results[0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+/* TEST POST DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPostDetail() : void
+    {
+        $o = new ContactRoles();
+        $result = $this->postDetail();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        //CLEAN UP
+        $test = $this->deleteDetail( $result['body']->id );
+    }
+
+
+
+/* TEST POST LIST
+---------------------------------------------------------------------------- */
+
+    public function testPostList() :void
+    {
+        $o = new ContactRoles();
+        $result = $o->postList(
+        data: [
+            [ 'name' => 'testContactRole1', 'slug' => 'aaa' ],
+            [ 'name' => 'testContactRole2', 'slug' => 'bbb' ],
+        ]  
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+
+        //CLEAN UP
+        foreach( $result['body'] AS $role )
+        {
+            $this->deleteDetail( id: $role->id );
+        }
+    }
+
+
+
+/* TEST PUT DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPutDetail() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->putDetail( 
+              id: $role->id, 
+            name: 'updateContactRole', 
+            slug: 'updateContactRole',
+            data: [ 'description' => 'Updated description' ]
+        );
+        
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+/* TEST PUT LIST
+---------------------------------------------------------------------------- */
+
+    public function testPutList() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->putList(
+            data: [
+                [ 
+                           'id'   => $role->id, 
+                           'name' => 'putContactRole',
+                           'slug' => 'putContactRole',
+                    'description' => 'Updated description'
+                ]
+            ]
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+/* TEST PATCH DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPatchDetail() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->patchDetail(
+              id: $role->id,
+            name: 'patchContactRole',
+            slug: 'patchContactRole',
+            data: [ 'description' => 'ContactRole test' ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+/* TEST PATCH LIST
+---------------------------------------------------------------------------- */
+
+    public function testPatchList() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->patchList(
+            data: [
+                [ 
+                          'id' => $role->id, 
+                        'name' => 'patchContactRole',
+                        'slug' => 'patchContactRole',
+                 'description' => 'patchContactRole' 
+                ]
+            ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $role->id );
+    }
+
+
+
+
+/* TEST DELETE DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testDeleteDetail() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+        
+        $o = new ContactRoles();
+        $result = $o->deleteDetail( id: $role->id );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* TEST DELETE LIST
+---------------------------------------------------------------------------- */
+
+    public function testDeleteList() : void
+    {
+        // SETUP
+        $role = $this->postDetail()['body'];
+
+        $o = new ContactRoles();
+        $result = $o->deleteList(
+            data: [[ 'id' => $role->id ]]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+/* CREATE A REGION
+---------------------------------------------------------------------------- */
+
+    public function postDetail() : array
+    {
+        $o = new ContactRoles();
+
+        return $o->postDetail( 
+            name: 'testContactRole',
+            slug: 'testContactRole',
+            data: [ 
+                'description' => 'PHPUnit test ContactRole',
+            ]
+        );
+    }
+
+
+
+/* DELETE A REGION
+---------------------------------------------------------------------------- */
+
+    public function deleteDetail( int $id )
+    {
+        $o = new ContactRoles();
+
+        return $o->deleteDetail( id: $id  );
+    }
+
+}
