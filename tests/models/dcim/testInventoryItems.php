72a56033bb2c7b42daf1eb22467b906cabb536e2
Colin Miller <ocolin@cruzio.com>
2023-02-03 14:09:32 -0500

    Added InventoryItems and tests

---
+++ tests/models/dcim/testInventoryItems.php
@@ -0,0 +1,407 @@
+<?php 
+
+declare( strict_types = 1 );
+
+namespace Cruzio\Netbox\Models\DCIM;
+
+use Cruzio\Netbox\Models\testCore;
+
+require_once __DIR__ . '/../testCore.php';
+
+class testInventoryItems extends testCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+/* TEST OPTIONS
+---------------------------------------------------------------------------- */
+
+    public function testOptions()
+    {
+        $o = new InventoryItems();
+        $result = $o->options();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'name', $result['body'] );
+    }
+
+
+/* TEST GET DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testGetDetail() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->getDetail( id: $item->id );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+ 
+
+
+/* TEST GET LIST
+---------------------------------------------------------------------------- */
+
+    public function testGetList() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->getList();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'results', $result['body'] );
+        $this->assertIsArray( $result['body']->results );
+        $this->assertObjectHasAttribute( 'id', $result['body']->results[0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+
+
+
+/* TEST POST DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPostDetail() : void
+    {
+        $o = new InventoryItems();
+        $result = $this->postDetail();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        //CLEAN UP
+        $this->deleteDetail( $result['body']->id );
+    }
+
+
+
+/* TEST POST LIST
+---------------------------------------------------------------------------- */
+
+    public function testPostList() :void
+    {
+        $o = new InventoryItems();
+        $result = $o->postList(
+            options: [[ 
+                      'name' => 'PHPUnit_InvItem',
+                    'device' => $_ENV['device']->id
+                ]] 
+            );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+
+        //CLEAN UP
+        foreach( $result['body'] AS $item )
+        {
+            $this->deleteDetail( id: $item->id );
+        }
+    }
+
+
+
+/* TEST PUT DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPutDetail() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->putDetail( 
+                id: $item->id, 
+              name: 'PHPUnit_InvItem',
+            device: $_ENV['device']->id                 
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+
+
+
+/* TEST PUT LIST
+---------------------------------------------------------------------------- */
+
+    public function testPutList() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->putList(
+            options: [
+                [ 
+                        'id' => $item->id,
+                      'name' => 'PHPUnit_InvItem',
+                    'device' => $_ENV['device']->id
+                ]
+            ]
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+
+
+
+/* TEST PATCH DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPatchDetail() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->patchDetail(
+                id: $item->id, 
+              name: 'PHPUnit_InvItem',
+            device: $_ENV['device']->id 
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+
+
+
+/* TEST PATCH LIST
+---------------------------------------------------------------------------- */
+
+    public function testPatchList() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->patchList(
+            options: [
+                [ 
+                        'id' => $item->id,
+                      'name' => 'PHPUnit_InvItem',
+                    'device' => $_ENV['device']->id
+                ]
+            ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $item->id );
+    }
+
+
+
+/* TEST DELETE DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testDeleteDetail() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+        
+        $o = new InventoryItems();
+        $result = $o->deleteDetail( id: $item->id );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* TEST DELETE LIST
+---------------------------------------------------------------------------- */
+
+    public function testDeleteList() : void
+    {
+        // SETUP
+        $item = $this->postDetail()['body'];
+
+        $o = new InventoryItems();
+        $result = $o->deleteList(
+            options: [[ 'id' => $item->id ]]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* CREATE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function postDetail() : array
+    {
+        $o = new InventoryItems();
+
+        return $o->postDetail( 
+            name: 'PHPUnit_InvItem',
+            device: $_ENV['device']->id
+        );
+    }
+
+
+
+/* DELETE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function deleteDetail( int $id )
+    {
+        $o = new InventoryItems();
+
+        return $o->deleteDetail( id: $id  );
+    }
+
+
+
+/* SETUP AND CLOSING FUNCTIONS
+---------------------------------------------------------------------------- */
+
+/**
+* @beforeClass
+*/
+    public static function setupTest()
+    {
+        $_ENV['site']     = self::createSite();
+        $_ENV['manf']     = self::createManufacturer();
+        $_ENV['tenant']   = self::createTenant();
+        $_ENV['devtype']  = self::createDeviceType( manf: $_ENV['manf'] );
+        $_ENV['location'] = self::createLocation( site: $_ENV['site'] );
+        $_ENV['devrole']  = self::createDeviceRole();
+        $_ENV['vc']       = self::createVirtualChassis();
+        $_ENV['rack']     = self::createRack( 
+            site: $_ENV['site'], location: $_ENV['location'] 
+        );
+        $_ENV['device']   = self::createDevice(
+                       site: $_ENV['site'],
+                     tenant: $_ENV['tenant'],
+                 devicetype: $_ENV['devtype'],
+                 devicerole: $_ENV['devrole'],
+            virtual_chassis: $_ENV['vc'],
+                       rack: $_ENV['rack']
+        );
+    }
+
+/**
+* @afterClass
+*/
+    public static function closeTest()
+    {
+        self::destroyDevice( device: $_ENV['device'] );
+        self::destroyRack( rack: $_ENV['rack'] );
+        self::destroyVirtualChassis( chassis: $_ENV['vc'] );
+        self::destroyDeviceRole( devrole: $_ENV['devrole'] );
+        self::destroyLocation( location: $_ENV['location'] );
+        self::destroyDeviceType( devtype: $_ENV['devtype'] );
+        self::destroyTenant( tenant: $_ENV['tenant'] );
+        self::destroyManufacturer( manf: $_ENV['manf'] );
+        self::destroySite( site: $_ENV['site'] );
+
+        unset( $_ENV['rack'] );
+        unset( $_ENV['vc'] );
+        unset( $_ENV['devrole'] );
+        unset( $_ENV['location'] );
+        unset( $_ENV['devtype'] );
+        unset( $_ENV['tenant'] );
+        unset( $_ENV['manf'] );
+        unset( $_ENV['site'] );
+        unset( $_ENV['device'] );
+    }
+}
