0ba3314f08d74ae85b2c26c894205bead5323933
Colin Miller <ocolin@cruzio.com>
2023-02-03 14:00:47 -0500

    Added InterfaceTemplates and tests

---
+++ tests/models/dcim/testInterfaceTemplates.php
@@ -0,0 +1,383 @@
+<?php 
+
+declare( strict_types = 1 );
+
+namespace Cruzio\Netbox\Models\DCIM;
+
+use Cruzio\Netbox\Models\testCore;
+
+require_once __DIR__ . '/../testCore.php';
+
+class testInterfaceTemplates extends testCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+/* TEST OPTIONS
+---------------------------------------------------------------------------- */
+
+    public function testOptions()
+    {
+        $o = new InterfaceTemplates();
+        $result = $o->options();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'name', $result['body'] );
+    }
+
+
+/* TEST GET DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testGetDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->getDetail( id: $template->id );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+ 
+
+
+/* TEST GET LIST
+---------------------------------------------------------------------------- */
+
+    public function testGetList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->getList();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'results', $result['body'] );
+        $this->assertIsArray( $result['body']->results );
+        $this->assertObjectHasAttribute( 'id', $result['body']->results[0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST POST DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPostDetail() : void
+    {
+        $o = new InterfaceTemplates();
+        $result = $this->postDetail();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        //CLEAN UP
+        $this->deleteDetail( $result['body']->id );
+    }
+
+
+
+/* TEST POST LIST
+---------------------------------------------------------------------------- */
+
+    public function testPostList() :void
+    {
+        $o = new InterfaceTemplates();
+        $result = $o->postList(
+            options: [[ 
+                           'name' => 'PHPUnit_INTF_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => 'virtual'
+                ]] 
+            );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+
+        //CLEAN UP
+        foreach( $result['body'] AS $template )
+        {
+            $this->deleteDetail( id: $template->id );
+        }
+    }
+
+
+
+/* TEST PUT DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPutDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->putDetail( 
+                     id: $template->id, 
+                   name: 'PHPUnit_INTF_Temp',
+            device_type: $_ENV['devtype']->id,
+                   type: 'virtual'             
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PUT LIST
+---------------------------------------------------------------------------- */
+
+    public function testPutList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->putList(
+            options: [
+                [ 
+                             'id' => $template->id,
+                           'name' => 'PHPUnit_INTF_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => 'virtual'
+                ]
+            ]
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PATCH DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPatchDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->patchDetail(
+                     id: $template->id, 
+                   name: 'PHPUnit_INTF_Temp',
+            device_type: $_ENV['devtype']->id ,
+                   type: 'virtual'
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PATCH LIST
+---------------------------------------------------------------------------- */
+
+    public function testPatchList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->patchList(
+            options: [
+                [ 
+                             'id' => $template->id,
+                           'name' => 'PHPUnit_INTF_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => 'virtual'
+                ]
+            ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST DELETE DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testDeleteDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+        
+        $o = new InterfaceTemplates();
+        $result = $o->deleteDetail( id: $template->id );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* TEST DELETE LIST
+---------------------------------------------------------------------------- */
+
+    public function testDeleteList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new InterfaceTemplates();
+        $result = $o->deleteList(
+            options: [[ 'id' => $template->id ]]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* CREATE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function postDetail() : array
+    {
+        $o = new InterfaceTemplates();
+
+        return $o->postDetail( 
+            name: 'PHPUnit_INTF_Temp',
+            device_type: $_ENV['devtype']->id,
+            type: 'virtual'
+        );
+    }
+
+
+
+/* DELETE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function deleteDetail( int $id )
+    {
+        $o = new InterfaceTemplates();
+
+        return $o->deleteDetail( id: $id  );
+    }
+
+
+
+/* SETUP AND CLOSING FUNCTIONS
+---------------------------------------------------------------------------- */
+
+/**
+* @beforeClass
+*/
+    public static function setupTest()
+    {
+        $_ENV['manf']     = self::createManufacturer();
+        $_ENV['devtype']  = self::createDeviceType( manf: $_ENV['manf'] );
+    }
+
+/**
+* @afterClass
+*/
+    public static function closeTest()
+    {
+        self::destroyDeviceType( devtype: $_ENV['devtype'] );
+        self::destroyManufacturer( manf: $_ENV['manf'] );
+
+        unset( $_ENV['devtype'] );
+        unset( $_ENV['manf'] );
+    }
+}
