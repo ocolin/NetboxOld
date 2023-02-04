9526846f22ddf9de7fbbb91f6179b814759172c1
Colin Miller <ocolin@cruzio.com>
2023-02-03 20:30:08 -0500

    Added model RearPortTemplates and tests

--- src/Models/DCIM/RearPortTemplates.php
+++ src/Models/DCIM/RearPortTemplates.php
@@ -15,5 +15,112 @@ class RearPortTemplates extends DCIM
     }
 
 
+/* POST METHOD DETAIL
+---------------------------------------------------------------------------- */
 
+/*
+* Create a single Template.
+*
+* @param string  $name Name of Template.
+* @param integer $device ID of Device type the Template belongs to. 
+* @param string  $type Port connector type.
+* @param array   $options optionsl data to be sent.
+* @param array   $headers HTML request headers.
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function postDetail(
+           int $device_type,
+        string $name,
+        string $type,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $options['name']        = $name;
+        $options['device_type'] = $device_type;
+        $options['type']        = $type;
+
+        return $this->http->post(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
+
+
+
+/* PUT METHOD DETAIL
+---------------------------------------------------------------------------- */
+
+/*
+* Update Template
+* 
+* @param integer $id Numerical ID of Template to update.
+* @param string  $name Name of Template.
+* @param integer $device ID of Device type the Template belongs to. 
+* @param string  $type Port connector type.
+* @param array   $options optional data to be sent.
+* @param array   $headers HTML request headers.
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function putDetail(
+           int $id,
+        string $name,
+        string $type,
+           int $device_type,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $this->uri .= "{$id}/";
+        $options['name']        = $name;
+        $options['device_type'] = $device_type;
+        $options['type']        = $type;
+
+        return $this->http->put(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
+
+
+
+/* PATCH METHOD DETAIL
+---------------------------------------------------------------------------- */
+
+/*
+* Update Template value(s).
+*
+* @param integer $id Numerical ID of Template to update.
+* @param string  $name Name of Template.
+* @param integer $device ID of Device type the Template belongs to.
+* @param string  $type Port connector type. 
+* @param array   $options optional data to be sent.
+* @param array   $headers HTML request headers.
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function patchDetail(
+           int $id,
+        string $name,
+        string $type,
+           int $device_type,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $this->uri .= "{$id}/";
+        $options['name']        = $name;
+        $options['device_type'] = $device_type;
+        $options['type']        = $type;
+
+        return $this->http->put(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
 }
---
+++ tests/models/dcim/testRearPortTemplates.php
@@ -0,0 +1,383 @@
+<?php 
+
+declare( strict_types = 1 );
+
+namespace Cruzio\Netbox\Models\DCIM;
+
+use Cruzio\Netbox\Models\testCore;
+
+require_once __DIR__ . '/../testCore.php';
+
+class testRearPortTemplates extends testCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+/* TEST OPTIONS
+---------------------------------------------------------------------------- */
+
+    public function testOptions()
+    {
+        $o = new RearPortTemplates();
+        $result = $o->options();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'name', $result['body'] );
+    }
+
+
+/* TEST GET DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testGetDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->getDetail( id: $template->id );   
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+ 
+
+
+/* TEST GET LIST
+---------------------------------------------------------------------------- */
+
+    public function testGetList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->getList();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'results', $result['body'] );
+        $this->assertIsArray( $result['body']->results );
+        $this->assertObjectHasAttribute( 'id', $result['body']->results[0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST POST DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPostDetail() : void
+    {
+        $o = new RearPortTemplates();
+        $result = $this->postDetail();
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        //CLEAN UP
+        $this->deleteDetail( $result['body']->id );
+    }
+
+
+
+/* TEST POST LIST
+---------------------------------------------------------------------------- */
+
+    public function testPostList() :void
+    {
+        $o = new RearPortTemplates();
+        $result = $o->postList(
+            options: [[ 
+                           'name' => 'PHPUnit_RP_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => '8p8c'
+                ]] 
+            );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 201, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+
+        //CLEAN UP
+        foreach( $result['body'] AS $template )
+        {
+            $this->deleteDetail( id: $template->id );
+        }
+    }
+
+
+
+/* TEST PUT DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPutDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->putDetail( 
+                     id: $template->id, 
+                   name: 'PHPUnit_RP_Temp',
+            device_type: $_ENV['devtype']->id,
+                   type: '8p8c'                
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PUT LIST
+---------------------------------------------------------------------------- */
+
+    public function testPutList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->putList(
+            options: [
+                [ 
+                             'id' => $template->id,
+                           'name' => 'PHPUnit_RP_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => '8p8c'
+                ]
+            ]
+        );
+        
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PATCH DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testPatchDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->patchDetail(
+                     id: $template->id, 
+                   name: 'PHPUnit_RP_Temp',
+            device_type: $_ENV['devtype']->id ,
+                   type: '8p8c'
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsObject( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST PATCH LIST
+---------------------------------------------------------------------------- */
+
+    public function testPatchList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->patchList(
+            options: [
+                [ 
+                             'id' => $template->id,
+                           'name' => 'PHPUnit_RP_Temp',
+                    'device_type' => $_ENV['devtype']->id,
+                           'type' => '8p8c'
+                ]
+            ]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 200, $result['status'] );
+        $this->assertIsArray( $result['headers'] );
+        $this->assertIsArray( $result['body'] );
+        $this->assertObjectHasAttribute( 'id', $result['body'][0] );
+
+        // CLEAN UP
+        $this->deleteDetail( $template->id );
+    }
+
+
+
+/* TEST DELETE DETAIL
+---------------------------------------------------------------------------- */
+
+    public function testDeleteDetail() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+        
+        $o = new RearPortTemplates();
+        $result = $o->deleteDetail( id: $template->id );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* TEST DELETE LIST
+---------------------------------------------------------------------------- */
+
+    public function testDeleteList() : void
+    {
+        // SETUP
+        $template = $this->postDetail()['body'];
+
+        $o = new RearPortTemplates();
+        $result = $o->deleteList(
+            options: [[ 'id' => $template->id ]]
+        );
+
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'status',  $result );
+        $this->assertArrayHasKey( 'headers', $result );
+        $this->assertArrayHasKey( 'body',    $result );
+        $this->assertIsInt( $result['status'] );
+        $this->assertEquals( 204, $result['status'] );
+    }
+
+
+
+/* CREATE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function postDetail() : array
+    {
+        $o = new RearPortTemplates();
+
+        return $o->postDetail( 
+                   name: 'PHPUnit_RP_Temp',
+            device_type: $_ENV['devtype']->id,
+                   type: '8p8c'
+        );
+    }
+
+
+
+/* DELETE A RACK ROLES
+---------------------------------------------------------------------------- */
+
+    public function deleteDetail( int $id )
+    {
+        $o = new RearPortTemplates();
+
+        return $o->deleteDetail( id: $id  );
+    }
+
+
+
+/* SETUP AND CLOSING FUNCTIONS
+---------------------------------------------------------------------------- */
+
+/**
+* @beforeClass
+*/
+    public static function setupTest()
+    {
+        $_ENV['manf']     = self::createManufacturer();
+        $_ENV['devtype']  = self::createDeviceType( manf: $_ENV['manf'] );
+    }
+
+/**
+* @afterClass
+*/
+    public static function closeTest()
+    {
+        self::destroyDeviceType( devtype: $_ENV['devtype'] );
+        self::destroyManufacturer( manf: $_ENV['manf'] );
+
+        unset( $_ENV['devtype'] );
+        unset( $_ENV['manf'] );
+    }
+}
