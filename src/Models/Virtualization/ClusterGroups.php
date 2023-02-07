d28f8e1b22aa2471a3a06cc05f1c5018b8f6def9
Colin Miller <ocolin@cruzio.com>
2023-02-06 22:00:33 -0500

    Added model ClusterGroups and tests

--- src/Models/Virtualization/ClusterGroups.php
+++ src/Models/Virtualization/ClusterGroups.php
@@ -2,11 +2,11 @@
 
 declare( strict_types = 1 );
 
-namespace Cruzio\Netbox\Models\Virtualizations;
+namespace Cruzio\Netbox\Models\Virtualization;
 
 use Cruzio\Netbox\Models\HTTP;
 
-class ClusterGroups extends Virtualizations
+class ClusterGroups extends Virtualization
 {
 
 /*
@@ -20,4 +20,104 @@ class ClusterGroups extends Virtualizations
 
 
 
+/* POST METHOD DETAIL
+---------------------------------------------------------------------------- */
+
+/*
+* Create a single Platform.
+*
+* @param string $name Name of Platform.
+* @param string $slug 
+* @param array  $options optionsl data to be sent
+* @param array $headers HTML request headers
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function postDetail(
+        string $name,
+        string $slug,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $options['name'] = $name;
+        $options['slug'] = $slug;
+
+        return $this->http->post(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
+
+
+
+/* PUT METHOD DETAIL
+---------------------------------------------------------------------------- */
+
+/*
+* Update Platform
+* 
+* @param integer $id Numerical ID of Platform to update.
+* @param string  $name Name of Platform to update.
+* @param string  $slug Slug of Platform to update.
+* @param array   $options Optional data to send.
+* @param array   $headers HTML request headers
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function putDetail(
+           int $id,
+        string $name,
+        string $slug,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $this->uri .= "{$id}/";
+        $options['name'] = $name;
+        $options['slug'] = $slug;
+
+        return $this->http->put(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
+
+
+
+/* PATCH METHOD DETAIL
+---------------------------------------------------------------------------- */
+
+/*
+* Update Platform value(s).
+*
+* @param integer $id Numerical ID of Platform to update.
+* @param string  $name Name of Platform to update.
+* @param string  $slug Slug of Platform to update.
+* @param array   $options Optional data to modify.
+* @param array   $headers HTML request headers.
+* @return array Array of HTTP status, headers, and body from Netbox API.
+*/
+
+    public function patchDetail(
+           int $id,
+        string $name,
+        string $slug,
+         array $options = [],
+         array $headers = []
+    ) : array
+    {
+        $this->uri .= "{$id}/";
+        $options['name'] = $name;
+        $options['slug'] = $slug;
+
+
+        return $this->http->put(
+                uri: $this->uri,
+               body: $options,
+            headers: $headers
+        );
+    }
 }
