eff9567a3f13fc9b59d9e6e8adbe5b31aaa56fc0
Colin Miller <ocolin@cruzio.com>
2023-02-08 19:40:45 -0500

    Added model PowerPortTemplates and tests

--- src/Models/DCIM/PowerPortTemplates.php
+++ src/Models/DCIM/PowerPortTemplates.php
@@ -11,7 +11,7 @@ class PowerPortTemplates extends DCIM
     public function __construct( HTTP $http = null )
     {
         parent::__construct( http: $http );
-        $this->uri .= 'port-port-templates/';
+        $this->uri .= 'power-port-templates/';
     }
 
 
